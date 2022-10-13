a10a74a10c0fccbeec01c9a04cc2b1c7e3787b9c
Никита Пермяков <permikov134@mail.ru>
2022-10-14 01:04:15 +0300

    Add select from db and view

--- server/server.go
+++ server/server.go
@@ -1,40 +1,57 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+var err error
 
-type HistoryItem struct {
+type Change struct {
 	Timestamp uint64 `json:"timestamp"`
 	Value float32 `json:"value"`
 }
 
-type Changes struct {
-	Total uint32 `json:"total"`
-	History []HistoryItem `json:"history_item"`
-}
+// type Changes struct {
+// 	Total uint32 `json:"total"`
+// 	History []Change `json:"history"`
+// }
+
+
 
 
 func view_last_change(w http.ResponseWriter, r *http.Request) {
-	change := &Changes{
-		Total: 1232,
-		History: []HistoryItem{
-			{
-				Timestamp: 12321434,
-				Value: 123.45,
-			},
-			{
-				Timestamp: 12321434,
-				Value: 123.45,
-			},
-		},
+	w.Header().Set("Content-Type", "application/json")
+	result, err := db.Query("SELECT timestamp, value from changes")
+	if err != nil {
+		panic(err.Error())
 	}
-	json.NewEncoder(w).Encode(change)
+	defer result.Close()
+	var change Change
+	var changes []Change
+	for result.Next() {
+		err := result.Scan(&change.Timestamp, &change.Value)
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(change.Timestamp)
+		changes = append(changes, change)
+	}
+	json.NewEncoder(w).Encode(changes)
 }
 
 func main() {
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/demo")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
 	http.HandleFunc("/api/btcusdt", view_last_change)
 	http.ListenAndServe(":8080", nil)
 }
