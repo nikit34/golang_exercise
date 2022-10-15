b031252b496dea18248f733bd53601bcc18a4915
Никита Пермяков <permikov134@mail.ru>
2022-10-15 09:31:43 +0300

    Add select count rows and separate on GET and POST

--- server/server.go
+++ server/server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -17,31 +16,49 @@ type Change struct {
 	Value float32 `json:"value"`
 }
 
-// type Changes struct {
-// 	Total uint32 `json:"total"`
-// 	History []Change `json:"history"`
-// }
+type Changes struct {
+	Total uint32 `json:"total"`
+	History []Change `json:"history"`
+}
 
 
 
 
-func view_last_change(w http.ResponseWriter, r *http.Request) {
+func view_btcusdt(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	result, err := db.Query("SELECT timestamp, value from changes")
+	var changes Changes
+
+	result_total, err := db.Query("SELECT COUNT(*) FROM changes")
 	if err != nil {
 		panic(err.Error())
 	}
-	defer result.Close()
-	var change Change
-	var changes []Change
-	for result.Next() {
-		err := result.Scan(&change.Timestamp, &change.Value)
+	for result_total.Next() {
+		err := result_total.Scan(&changes.Total)
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Println(change.Timestamp)
-		changes = append(changes, change)
 	}
+	defer result_total.Close()
+
+	switch r.Method {
+	case "GET":
+		result_history, err := db.Query("SELECT timestamp, value from changes ORDER BY timestamp DESC LIMIT 1")
+		if err != nil {
+			panic(err.Error())
+		}
+		defer result_history.Close()
+		var change Change
+		for result_history.Next() {
+			err := result_history.Scan(&change.Timestamp, &change.Value)
+			if err != nil {
+				panic(err.Error())
+			}
+			changes.History = append(changes.History, change)
+		}
+	case "POST":
+
+	}
+
 	json.NewEncoder(w).Encode(changes)
 }
 
@@ -52,6 +69,6 @@ func main() {
 	}
 	defer db.Close()
 
-	http.HandleFunc("/api/btcusdt", view_last_change)
+	http.HandleFunc("/api/btcusdt", view_btcusdt)
 	http.ListenAndServe(":8080", nil)
 }
